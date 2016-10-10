a3067e98fcf4e73aa982ecd8a7a737781e2ece46
luchenjie <luchenjie@luchenjies-MacBook-Pro.local>
2016-10-10 14:39:32 +0800

    update

--- app.js
+++ app.js
@@ -23,16 +23,18 @@ server.get('/georeport/:lat/:longi', function (req, res, next) {
 });
 
 server.get('/georeport/get', function (req, res, next) {
-	fetchUrl("http://restapi.amap.com/v3/direction/driving?output=json&origin=121.168105,31.303751&destination=121.264168,31.002682&output=xml&key=10d7b58e081067d38c01996686245507", function(error, meta, body){
-	    var resp = JSON.parse(body.toString());
-	    if(resp.status == 1 && parseInt(resp.count) > 0) {
-	    	var duration = parseInt(resp.route.paths[0].duration);
-	    	res.send(duration/60 + " minutes");
-	    } else {
-	    	res.send("lbs error");
-	    }
-  		return next();
-	});
+	if(longitude && latitude) {
+		fetchUrl("http://restapi.amap.com/v3/direction/driving?output=json&origin="+longitude+","+latitude+"&destination=121.264168,31.002682&output=xml&key=10d7b58e081067d38c01996686245507", function(error, meta, body){
+		    var resp = JSON.parse(body.toString());
+		    if(resp.status == 1 && parseInt(resp.count) > 0) {
+		    	var duration = parseInt(resp.route.paths[0].duration);
+		    	res.send(duration/60 + " minutes");
+		    } else {
+		    	res.send("lbs error");
+		    }
+	  		return next();
+		});
+	}
 });
  
 server.listen(8080, function () {
